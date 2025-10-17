3c0e217b7614b55e1f36bfb301da23bb4f54800c
pavobravo11 <gustavoa1011@gmail.com>
2025-10-17 00:15:28 -0600

    Added copy_data script, for the script to run, the database must be setup on the users postgre server

---
+++ database/create_tables.sql
@@ -0,0 +1,11 @@
+-- Run this command first
+CREATE TABLE carmichael_number (
+    number NUMERIC(24, 0) PRIMARY KEY,
+    factors NUMERIC(20, 0)[] NOT NULL
+);
+
+-- After the batch insert, run this command, prevent insert slowdown
+CREATE INDEX factors_index ON carmichael_number USING GIN (factors);
+
+-- In case you need a reset
+DROP TABLE IF EXISTS carmichael_number;
