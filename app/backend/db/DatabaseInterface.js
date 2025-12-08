805447247730ca5a2e0e4c7e84d36a007208068a
Gustavo Bravo <gustavoa1011@gmail.com>
2025-12-08 16:38:20 -0500

    created abstract class for backend impleentation

--- app/backend/db/DatabaseInterface.js
+++ app/backend/db/DatabaseInterface.js
@@ -7,5 +7,35 @@
  * There will only be two methods to start off, get Carmichael numbers based on a list of numbers (1-14)
  * and get the factors of a carmichael number by simply looking for it.
  * 
- * For safety, we have to concern ourselves with SQL injection, as our final DB my be a SQL database.
+ * @author Gustavo Bravo
+ * @date December 8, 202
  */
+class DatabaseInterface {
+    constructor() {
+        if (this.target === DatabaseInterface) {
+            throw new Error("Abstract types cannot be instatiated.");
+        }
+    }
+
+    /**
+     * Get CN number based on a list of prime factors (no primality test done)
+     * @param {number[]} factors - Prime factors
+     * @returns {Promise<bigint[]>} CN numbers that pass the divisibility test
+     * @throws {Error} If not implemented by concrete class
+     */
+    async getCarmichaelNumber(factors) {
+        throw new Error("Method 'getCarmichaelNumber()' must be implemented");
+    }
+
+    /**
+     * Get the factors of a number if it is a CN number
+     * @param {number} carmichael_number
+     * @returns {Promise<number[]>} factors of CN number
+     * @throws {Error} If not implemented by concrete class
+     */
+    async getFactors(carmichael_numbernumber) {
+        throw new Error("Method 'getFactors' must be implement")
+    }
+}
+
+MediaSourceHandle.exports = DatabaseInterface;
