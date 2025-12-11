03c19a230902ab74f089afe4df5c82dc77b3e307
Gustavo Bravo <gustavoa1011@gmail.com>
2025-12-11 17:52:50 -0500

    Updated mongo insertion script to validate types, added mongo_view notebook to look inside the database since we don't have a client

--- database/mongo_insert.py
+++ database/mongo_insert.py
@@ -13,39 +13,57 @@ import os
 
 from dotenv import load_dotenv
 from pymongo import MongoClient
+from bson import Decimal128
 
 
 load_dotenv(override=True)
 
 FILENAME = os.getenv('FILE_LOCATION', '')
+CHECKPOINT = 1000000
 
 def insert_data():
     user = urllib.parse.quote_plus(os.getenv('MONGO_USER', ''))
     password = urllib.parse.quote_plus(os.getenv('MONGO_PASSWORD', ''))
-    uri = f"mongodb://{user}:{password}@localhost:27017/"
+    host = urllib.parse.quote_plus(os.getenv('MONGO_HOST', ''))
+    port = urllib.parse.quote_plus(os.getenv('MONGO_PORT', '8000'))
+
+    uri = f"mongodb://{user}:{password}@{host}:{port}/"
 
     db_name = os.getenv('DATABASE', '')
     if not db_name:
         raise ValueError("Provide a non-empty db name.")
+    
+    row_count = 0
 
     # Context manager handles closing if failure
     with MongoClient(uri) as client:
-        with open(FILENAME, encoding='utf-8') as file:
-            db = client[db_name]
-            cn_collection = db['carmichael_number']
+        db = client[db_name]
 
+        # Enforce a schema for our new collection
+        cn_collection = db.create_collection(
+            "carmichael_number", 
+            validator={
+                    "_id": {"$type": "decimal"},
+                    "factors": {"$type": "array"}
+            },
+            validationLevel="strict"
+        )
+
+        with open(FILENAME, encoding='utf-8') as file:
             for line in file:
                 parts = line.strip().split(' ')
 
                 cn_collection.insert_one({
-                    "number": parts[0].strip(),
-                    "factors": [factor for factor in parts[1:]]
+                    "_id": Decimal128(parts[0].strip()),
+                    "factors": [int(factor.strip()) for factor in parts[1:]]
                 })
+
+                row_count += 1
+                if row_count % CHECKPOINT == 0:
+                    print(f"Inserted {row_count/CHECKPOINT} million rows.")
         
         # Create an index on factors in ascending order (default)
         cn_collection.create_index("factors")
-
-
 
 
 if __name__ == "__main__":
