4c43eeebcb14a0aac3f2c77499b37e67ba62384a
Gustavo Bravo <gustavoa1011@gmail.com>
2025-12-12 05:11:06 -0500

    batch inserting added to mongo script, improved performance A LOT

--- database/mongo_insert.py
+++ database/mongo_insert.py
@@ -17,12 +17,13 @@ import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
 from bson import Decimal128
+from decimal import Decimal
 
 
 load_dotenv(override=True)
 
 FILENAME = os.getenv('FILE_LOCATION', '')
-CHECKPOINT = 1000000
+CHECKPOINT = 10000
 
 def insert_data():
     user = urllib.parse.quote_plus(os.getenv('MONGO_USER', ''))
@@ -37,33 +38,52 @@ def insert_data():
         raise ValueError("Provide a non-empty db name.")
     
     row_count = 0
+    batch = []
 
     # Context manager handles closing if failure
     with MongoClient(uri) as client:
         db = client[db_name]
 
-        # Enforce a schema for our new collection
-        cn_collection = db.create_collection(
-            "carmichael_number", 
-            validator={
-                    "_id": {"$type": "decimal"},
-                    "factors": {"$type": "array"}
-            },
-            validationLevel="strict"
-        )
+        if "carmichael_number" not in db.list_collection_names():
+            # Enforce a schema for our new collection
+            cn_collection = db.create_collection(
+                "carmichael_number", 
+                validator={
+                        "_id": {"$type": "decimal"},
+                        "factors": {"$type": "array"}
+                },
+                validationLevel="strict"
+            )
+            max_inserted = 0
+        else: # Look for checkpoint
+            cn_collection = db["carmichael_number"]
+            max_query = cn_collection.aggregate([
+                {
+                    "$group": {
+                        "_id": None,
+                        "max_carmichael": {"$max": "$_id"}
+                    }
+                }
+            ]).next()
+            max_inserted = Decimal(str(max_query['max_carmichael']))
 
         with open(FILENAME, encoding='utf-8') as file:
             for line in file:
                 parts = line.strip().split(' ')
 
-                cn_collection.insert_one({
+                if max_inserted >= Decimal(parts[0].strip()):
+                    continue
+
+                batch.append({
                     "_id": Decimal128(parts[0].strip()),
                     "factors": [int(factor.strip()) for factor in parts[1:]]
                 })
 
-                row_count += 1
-                if row_count % CHECKPOINT == 0:
-                    print(f"Inserted {row_count/CHECKPOINT} million rows.")
+                if len(batch) == CHECKPOINT:
+                    cn_collection.insert_many(batch, ordered=False)
+                    row_count += len(batch)
+                    batch = []
+                    print(f"Inserted {row_count} rows.")
         
         # Create an index on factors in ascending order (default)
         cn_collection.create_index("factors")
